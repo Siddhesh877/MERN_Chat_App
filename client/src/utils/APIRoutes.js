b1a9eba24043149ddac56f1f8cacfd47b24b81fd
Siddhesh Parate <siddheshparate877@gmail.com>
2023-07-12 16:24:43 +0530

    host updated

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host ="https://buzzchat.onrender.com";
+export const host ="https://buzzchatapp.netlify.app";
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute=`${host}/api/auth/setAvatar`;
